abc2b2fa786902df3695705567a6773ea74c6f9d
Faizu Rahman <www.faizu9264@gmail.com>
2025-07-24 23:45:07 -0700

    add output standalone to Next.js config

--- admin/next.config.ts
+++ admin/next.config.ts
@@ -16,6 +16,38 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
     ],
+    unoptimized: true,
+  },
+  output: 'standalone',
+  trailingSlash: false,
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ];
   },
 };
 

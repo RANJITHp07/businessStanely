9173e2e2ce5c03ccd41e6ce86bbfa38bbd80e3c6
Ranjith224 <ranjith.p@neurosparkworks.com>
2025-07-28 16:25:11 +0530

    changes in the schema and removed unecessary layouts

--- admin/src/app/layout.tsx
+++ admin/src/app/layout.tsx
@@ -1,9 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
-import Navigator from "@/components/navigator";
 import { ToastContainer } from 'react-toastify';
 
 
